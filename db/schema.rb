02ec26bdb700a1c0a6153a46007179cc4c466841
Ayush Jain <jain20.ayush@gmail.com>
2020-04-05 17:36:00 +0530

    add posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_110235) do
+ActiveRecord::Schema.define(version: 2020_04_05_110906) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -43,21 +43,12 @@ ActiveRecord::Schema.define(version: 2020_04_04_110235) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.string "name"
-    t.text "comment"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.text "content"
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
